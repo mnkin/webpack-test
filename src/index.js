bda21bfca2c43de58b1790b88be6b5c7a277d91c
mnkin <2664306866@qq.com>
2021-01-29 19:28:42 +0800

    submit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import x from "./x.js";
-console.log(x);
+import png from "./assets/1.png";
+
+const div = document.getElementById("app1");
+div.innerHTML = `
+    <img src="${png}">
+`;
+
+const btn = document.createElement("button");
+btn.innerText = "懒加载";
+btn.onclick = () => {
+  const promise = import("./lazy");
+  promise.then(
+    (module) => {
+      const fn = module.default;
+      fn();
+    },
+    () => {}
+  );
+};
+div.appendChild(btn);
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -13,4 +13,32 @@ module.exports = {
       template: "src/assets/index.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: ["file-loader"], //把文件变为文件路径
+      },
+      {
+        test: /\.styl$/i,
+        use: ["style-loader", "css-loader", "stylus-loader"],
+      },
+      {
+        test: /\.less$/i,
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader", //将js字符串转化为style标签
+          "css-loader", //将css代码转化为js字符串
+          "sass-loader", //将sass代码转换为css代码
+          //   {
+          //     loader: "sass-loader",
+          //     implementation: require("dart-sass"),
+          //   },
+        ],
+      },
+    ],
+  },
 };
